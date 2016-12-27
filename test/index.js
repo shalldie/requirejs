1c1af84b7de32818fad68afefb9a5002d3eb80a8
谢爽 <394229785@qq.com>
2016-12-27 16:07:16 +0800

    add test

--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ require([
     'module/item2',
     './tool/tool'
 ], function (helloworld, item1, item2, _) {
-    var result = helloworld + "<br><br>item2 的原始值：<br><br>" + item1;
+    var result = helloworld + "<br><br>-----------------------------------------------<br><br>item2 的原始值：<br><br>" + item1;
 
-    result += "在 item1 中处理之后，item2 的现有值为：<br><br>";
+    result += "<br><br>-----------------------------------------------<br><br>在 item1 中处理之后，item2 的现有值为：<br><br>";
 
     _.each(item2, function (k, v) {
         result += k + ": " + v + " <br><br>";
